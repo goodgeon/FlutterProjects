99da73220529b14ed9483efac677d5418453142f
goodgeon <dlrnerjs@gmail.com>
2023-09-25 23:49:15 +0900

    dark theme

--- 3_expense_tracker/lib/widgets/expenses_list/expenses_list.dart
+++ 3_expense_tracker/lib/widgets/expenses_list/expenses_list.dart
@@ -15,8 +15,10 @@ class ExpensesList extends StatelessWidget {
       itemCount: expenses.length,
       itemBuilder: (context, index) => Dismissible(
         background: Container(
-          color: Theme.of(context).colorScheme.error,
-          margin: Theme.of(context).cardTheme.margin,
+          color: Theme.of(context).colorScheme.error.withOpacity(0.75),
+          margin: EdgeInsets.symmetric(
+            horizontal: Theme.of(context).cardTheme.margin!.horizontal,
+          ),
         ),
         key: ValueKey(expenses[index]),
         child: ExpenseItem(expenses[index]),
