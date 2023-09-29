978670b92262225ca34e3f9b1440a25af70ed50b
goodgeon <dlrnerjs@gmail.com>
2023-09-30 00:57:29 +0900

    LayoutBuilder / Platform check

--- 3_expense_tracker/lib/widgets/new_expense/new_expense.dart
+++ 3_expense_tracker/lib/widgets/new_expense/new_expense.dart
@@ -1,4 +1,5 @@
 import 'dart:ffi';
+import 'dart:io';
 
 import 'package:expense_tracker/models/expense.dart';
 import 'package:expense_tracker/widgets/new_expense/amount_textfield.dart';
@@ -7,6 +8,7 @@ import 'package:expense_tracker/widgets/new_expense/category_dropdown.dart';
 import 'package:expense_tracker/widgets/new_expense/date_button.dart';
 import 'package:expense_tracker/widgets/new_expense/save_button.dart';
 import 'package:expense_tracker/widgets/new_expense/title_textfield.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class NewExpense extends StatefulWidget {
@@ -42,13 +44,27 @@ class _NewExpenseState extends State<NewExpense> {
     });
   }
 
-  void _submitExpenseData() {
-    final enteredAmount = double.tryParse(_amountController.text);
-    final amountIsInvalid = enteredAmount == null || enteredAmount <= 0;
-
-    if (_titleController.text.trim().isEmpty ||
-        amountIsInvalid ||
-        _selectedDate == null) {
+  void _showDialog() {
+    if (Platform.isIOS) {
+      showCupertinoDialog(
+        context: context,
+        builder: (context) {
+          return CupertinoAlertDialog(
+            title: const Text('Invalid input'),
+            content: const Text(
+                'Please make sure a valid title, amount, date and category was entered.'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Okay'),
+              )
+            ],
+          );
+        },
+      );
+    } else {
       showDialog(
           context: context,
           builder: (ctx) {
@@ -66,6 +82,17 @@ class _NewExpenseState extends State<NewExpense> {
               ],
             );
           });
+    }
+  }
+
+  void _submitExpenseData() {
+    final enteredAmount = double.tryParse(_amountController.text);
+    final amountIsInvalid = enteredAmount == null || enteredAmount <= 0;
+
+    if (_titleController.text.trim().isEmpty ||
+        amountIsInvalid ||
+        _selectedDate == null) {
+      _showDialog();
       return;
     }
 
@@ -90,38 +117,86 @@ class _NewExpenseState extends State<NewExpense> {
   Widget build(BuildContext context) {
     final keyboardSpace = MediaQuery.of(context).viewInsets.bottom;
 
-    return SizedBox(
-      height: double.infinity,
-      child: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.fromLTRB(16, 16, 16, keyboardSpace + 16),
-          child: Column(
-            children: [
-              TitleTextField(titleController: _titleController),
-              Row(
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final width = constraints.maxWidth;
+
+        return SizedBox(
+          height: double.infinity,
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(16, 16, 16, keyboardSpace + 16),
+              child: Column(
                 children: [
-                  Expanded(
-                    child: AmountTextField(
-                      amountController: _amountController,
+                  if (width >= 600)
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          child:
+                              TitleTextField(titleController: _titleController),
+                        ),
+                        const SizedBox(width: 24),
+                        Expanded(
+                          child: AmountTextField(
+                            amountController: _amountController,
+                          ),
+                        )
+                      ],
+                    )
+                  else
+                    TitleTextField(titleController: _titleController),
+                  const SizedBox(height: 16),
+                  if (width >= 600)
+                    Row(
+                      children: [
+                        CategoryDropdown(selectedCategory: _selectedCategory),
+                        const Spacer(),
+                        Expanded(
+                          child:
+                              DateButton(presentDatePicker: _presentDatePicker),
+                        ),
+                      ],
+                    )
+                  else
+                    Row(
+                      children: [
+                        Expanded(
+                          child: AmountTextField(
+                            amountController: _amountController,
+                          ),
+                        ),
+                        const SizedBox(width: 16),
+                        Expanded(
+                          child:
+                              DateButton(presentDatePicker: _presentDatePicker),
+                        ),
+                      ],
                     ),
-                  ),
-                  const SizedBox(width: 16),
-                  Expanded(
-                      child: DateButton(presentDatePicker: _presentDatePicker)),
+                  const SizedBox(height: 16),
+                  if (width >= 600)
+                    Row(
+                      children: [
+                        const Spacer(),
+                        const CancelButton(),
+                        SaveButton(submitExpenseData: _submitExpenseData)
+                      ],
+                    )
+                  else
+                    Row(
+                      children: [
+                        CategoryDropdown(selectedCategory: _selectedCategory),
+                        const Spacer(),
+                        const CancelButton(),
+                        SaveButton(submitExpenseData: _submitExpenseData),
+                      ],
+                    )
                 ],
               ),
-              Row(
-                children: [
-                  CategoryDropdown(selectedCategory: _selectedCategory),
-                  const Spacer(),
-                  const CancelButton(),
-                  SaveButton(submitExpenseData: _submitExpenseData),
-                ],
-              )
-            ],
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
