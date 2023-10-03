4d08e9d48bf82cd4d908de796e8aa1a54b355dfb
goodgeon <dlrnerjs@gmail.com>
2023-10-04 00:31:55 +0900

    making meals item as Card

--- 4_meals/lib/screens/meals.dart
+++ 4_meals/lib/screens/meals.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.title, required this.meals});
@@ -37,7 +38,7 @@ class MealsScreen extends StatelessWidget {
     if (meals.isNotEmpty) {
       content = ListView.builder(
         itemCount: meals.length,
-        itemBuilder: (context, index) => Text(meals[index].title),
+        itemBuilder: (context, index) => MealItem(meal: meals[index]),
       );
     }
 
